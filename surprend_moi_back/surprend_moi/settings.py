52232f2c0f9d999d7cb778bf6dc28a0185e38b10
Alexis Craye <alexis1.craye@epitech.eu>
2020-10-03 12:52:08 +0200

    adding cors policy on settings

--- surprend_moi_back/surprend_moi/settings.py
+++ surprend_moi_back/surprend_moi/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
     'api',
     'rest_framework'
 ]
@@ -44,6 +45,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    # 'corsheaders.middleware.common.CommonMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -133,7 +135,7 @@ REST_FRAMEWORK = {
 # Cors header
 CORS_ORIGIN_ALLOW_ALL = True
 
-from corsheaders.default import default_headers
+from corsheaders.defaults import default_headers
 
 CORS_ALLOW_HEADERS = list(default_headers) + [
     'token'
