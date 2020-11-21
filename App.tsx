8e99c5aa89c3d546116944ed5337fa4ded85cddc
Alexis Craye <alexis1.craye@epitech.eu>
2020-11-21 22:47:01 +0100

    feat: eslint on app

--- App.tsx
+++ App.tsx
@@ -7,22 +7,22 @@ import ListScreen from './screens/listscreen';
 import ListItemsScreen from './screens/listItemsScreen';
 import RegisterScreen from './screens/registerscreen';
 import CameraScreen from './screens/cameraScreen';
-import useColorScheme from './hooks/useColorScheme';
+// import useColorScheme from './hooks/useColorScheme';
 import { UserContext } from './contexts/userContext';
 import * as routes from './routes';
 
 const Stack = createStackNavigator();
 
 export default function App() {
-  const colorScheme = useColorScheme();
+  // const colorScheme = useColorScheme();
   const [token, setToken] = useState('');
 
-  const initialRouteName = token.length == 0 ? "HomePage" : "lists"
+  const initialRouteName = token.length === 0 ? routes.homeRoute : routes.listsRoute;
 
   return (
     <NavigationContainer>
       <UserContext.Provider value={{ token, setToken }}>
-        <Stack.Navigator colorScheme={colorScheme} initialRouteName={initialRouteName}>
+        <Stack.Navigator initialRouteName={initialRouteName}>
           <Stack.Screen name={routes.homeRoute} component={HomeScreen} options={{ title: 'Shopping list' }} />
           <Stack.Screen name={routes.registerRoute} component={RegisterScreen} />
           <Stack.Screen name={routes.loginRoute} component={LoginScreen} />
