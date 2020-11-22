5b173d3f64e9e82cc72156971660e9d52e7c9fe7
Theo <theo.clement@epitech.eu>
2020-11-22 15:41:33 +0100

    add readme

---
+++ Readme.mk
@@ -0,0 +1,11 @@
+To download the libraries needed for the app, use the command:
+
+$> expo install
+
+To use the develop mod of the app, use the command:
+
+$> expo start
+
+To publish the app in public access, use the command:
+
+$> expo publish
